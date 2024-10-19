4b45eef1144263368d912c4d8cc5c19d8a41bea3
Hassan Moechtar <zetenha@gmail.com>
2024-10-19 17:33:49 +0200

    mod EventsPage

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,16 +1,27 @@
 import React from "react";
-import { Heading } from "@chakra-ui/react";
+import { Heading, Image } from "@chakra-ui/react";
 import { useLoaderData, Link } from "react-router-dom";
 
 export const loader = async () => {
   const users = await fetch("http://localhost:3000/users");
   const events = await fetch("http://localhost:3000/events");
+  const categories = await fetch("http://localhost:3000/categories");
 
-  return { users: await users.json(), events: await events.json() };
+  return {
+    users: await users.json(),
+    events: await events.json(),
+    categories: await categories.json(),
+  };
 };
 
 export const EventsPage = () => {
-  const { users, events } = useLoaderData();
+  const { users, events, categories } = useLoaderData();
+
+  const getCategories = (categoryIds) => {
+    return categoryIds
+      .map((id) => categories.find((cat) => cat.id === id)?.name)
+      .join(", ");
+  };
 
   return (
     <div>
@@ -21,6 +32,10 @@ export const EventsPage = () => {
             <h2>{event.title}</h2>
           </Link>
           <p>{event.description}</p>
+          <Image src={event.image} alt={event.title} boxSize="150px" />
+          <p>Start Time: {new Date(event.startTime).toLocaleString()}</p>
+          <p>End Time: {new Date(event.endTime).toLocaleString()}</p>
+          <p>Categories: {getCategories(event.categoryIds)}</p>
           <p>
             Created by:{" "}
             {users.find((user) => event.createdBy === user.id)?.name}
