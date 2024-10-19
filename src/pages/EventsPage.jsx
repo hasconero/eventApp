51dda7192838cab8189e644e6ebde5f9c7b4179c
Hassan Moechtar <zetenha@gmail.com>
2024-10-19 17:06:43 +0200

    mod EventsPage

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 import { Heading } from "@chakra-ui/react";
+import { useLoaderData, Link } from "react-router-dom";
+
+export const loader = async () => {
+  const users = await fetch("http://localhost:3000/users");
+  const events = await fetch("http://localhost:3000/events");
+
+  return { users: await users.json(), events: await events.json() };
+};
 
 export const EventsPage = () => {
-  return <Heading>List of events</Heading>;
+  const { users, events } = useLoaderData();
+
+  return (
+    <div>
+      <Heading>List of events</Heading>
+      {events.map((event) => (
+        <div key={event.id}>
+          <Link to={`event/${event.id}`}>
+            <h2>{event.title}</h2>
+          </Link>
+          <p>{event.description}</p>
+          <p>
+            Created by:{" "}
+            {users.find((user) => event.createdBy === user.id)?.name}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 };
