2b73f15ff8f8315e6adebac43cc36ebab13bd6e6
Hassan Moechtar <zetenha@gmail.com>
2024-10-20 22:11:15 +0200

    MOD Navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Box, Flex, HStack, Link, Heading } from "@chakra-ui/react";
 
 export const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Events</Link>
-        </li>
-        <li>
-          <Link to="/event/1">Event</Link>
-        </li>
-      </ul>
-    </nav>
+    <Box bg="teal.500" px={4} mb={6}>
+      <Flex h={16} alignItems="center" justifyContent="space-between">
+        <Heading as={RouterLink} to="/" size="lg" color="white">
+          Event Manager
+        </Heading>
+
+        <HStack as="nav" spacing={4}>
+          <Link
+            as={RouterLink}
+            to="/"
+            color="white"
+            _hover={{ textDecoration: "none", color: "teal.200" }}
+          >
+            Events
+          </Link>
+          <Link
+            as={RouterLink}
+            to="/event/1"
+            color="white"
+            _hover={{ textDecoration: "none", color: "teal.200" }}
+          >
+            Event
+          </Link>
+        </HStack>
+      </Flex>
+    </Box>
   );
 };
