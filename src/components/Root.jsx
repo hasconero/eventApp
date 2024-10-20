ff5c5437690437262ab0eb292f92f01fd4b6a82d
Hassan Moechtar <zetenha@gmail.com>
2024-10-20 22:04:15 +0200

    MOD Root

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -5,8 +5,8 @@ import { Box, Center } from "@chakra-ui/react";
 
 export const Root = () => {
   return (
-    <Center>
-      <Box>
+    <Center w="100%" maxW="1200px" mx="auto" p={5}>
+      <Box w="100%">
         <Navigation />
         <Outlet />
       </Box>
