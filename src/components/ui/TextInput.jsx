943667d22e25ead17ed1cc35e1913d76735b625f
Hassan Moechtar <zetenha@gmail.com>
2024-10-20 15:34:09 +0200

    Mod TextInput

--- src/components/ui/TextInput.jsx
+++ src/components/ui/TextInput.jsx
@@ -1,5 +1,5 @@
-import { Input } from '@chakra-ui/react';
+import { Input } from "@chakra-ui/react";
 
 export const TextInput = ({ onChange, ...props }) => (
-	<Input variant="flushed" onChange={onChange} {...props} />
+  <Input size="md" variant="filled" onChange={onChange} {...props} />
 );
