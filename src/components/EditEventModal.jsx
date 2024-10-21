92d2bfdb59852a7533e84644f20fdc66d783c87b
Hassan Moechtar <zetenha@gmail.com>
2024-10-21 09:34:09 +0200

    ADD EditEventModal

---
+++ src/components/EditEventModal.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { Button } from "./ui/Button";
+import { EditEventForm } from "./EditEventForm";
+
+export const EditEventModal = ({
+  isOpen,
+  onClose,
+  event,
+  categories,
+  users,
+  onSubmit,
+}) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Edit Event</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          {/* Pass current event data to the form */}
+          <EditEventForm
+            event={event}
+            categories={categories}
+            users={users}
+            onSubmit={onSubmit}
+            onClose={onClose}
+          />
+        </ModalBody>
+
+        <ModalFooter>
+          <Button onClick={onClose}>Cancel</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
